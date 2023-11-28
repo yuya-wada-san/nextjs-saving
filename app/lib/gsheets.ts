de0f51974c0c9209dbe43bd295ec33a414ee8121
yuya-wada-san <yuya.wada.san@gmail.com>
2023-11-28 17:29:55 +1100

    add adjust text code

--- app/lib/gsheets.ts
+++ app/lib/gsheets.ts
@@ -5,7 +5,7 @@ export async function getGoogleSheetsData(range: string) {
     projectId: "prefab-fabric-406501",
     credentials: {
       type: "service_account",
-      private_key: process.env.GCP_SERVICEACCOUNT_PRIVATE_KEY,
+      private_key: process.env.GCP_SERVICEACCOUNT_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       client_email: process.env.GCP_SERVICEACCOUNT_EMAIL,
       client_id: "116113235585374941436",
       token_url: "https://oauth2.gooxgleapis.com/token",
