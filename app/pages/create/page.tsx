b3ba5ad251411e9460d240842f42e49285407966
yuya-wada-san <yuya.wada.san@gmail.com>
2023-12-31 11:34:34 +1100

    fix meta

--- app/pages/create/page.tsx
+++ app/pages/create/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Form',
+  title: 'Create',
 };
 
 export default async function Page() {
